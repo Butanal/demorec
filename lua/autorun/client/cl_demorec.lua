00c7395fbbe819f0de7a3d7b547e591178a904c3
Benjamin <bsepe@monaco.mc>
2019-02-08 13:22:57 +0100

    Reformat LUA code

--- lua/autorun/client/cl_demorec.lua
+++ lua/autorun/client/cl_demorec.lua
@@ -2,85 +2,79 @@ DemoRec = DemoRec or {}
 DemoRec.Queue = DemoRec.Queue or {}
 DemoRec.settings = DemoRec.settings or {}
 
-
 function DemoRec.PostDemo(data, CurrentRecord)
-	if not data or #data == 0 then return end
+    if not data or #data == 0 then
+        return
+    end
 
-	local post_table = {}
-	post_table["sid64"] = tostring(LocalPlayer():SteamID64())
-	post_table["filename"] = CurrentRecord.filename
-	post_table["cl_key"] = tostring(CurrentRecord.key)
-	post_table["data_b64"] = util.Base64Encode(data)
-	http.Post(DemoRec.settings.website, post_table, function(r, l, h, code)
-		if code == 200 then
-			net.Start("DemoRec.EndRequest")
-      			net.WriteBool(true)
-			net.SendToServer()
-		else
-			local err = util.JSONToTable(r)["message"]
-			net.Start("DemoRec.EndRequest")
-			net.WriteBool(false)
-			net.WriteString(err)
-			net.SendToServer()
-		end
-	end, function(err) 
-      net.Start("DemoRec.EndRequest")
-      net.WriteBool(false)
-	  net.WriteString(err)
-      net.SendToServer()
-  end )
+    local post_table = {}
+    post_table["sid64"] = tostring(LocalPlayer():SteamID64())
+    post_table["filename"] = CurrentRecord.filename
+    post_table["cl_key"] = tostring(CurrentRecord.key)
+    post_table["data_b64"] = util.Base64Encode(data)
+
+    http.Post(DemoRec.settings.website, post_table, function(r, l, h, code)
+        if code == 200 then
+            net.Start("DemoRec.EndRequest")
+            net.WriteBool(true)
+            net.SendToServer()
+        else
+            local err = util.JSONToTable(r)["message"]
+            net.Start("DemoRec.EndRequest")
+            net.WriteBool(false)
+            net.WriteString(err)
+            net.SendToServer()
+        end
+    end, function(err)
+        net.Start("DemoRec.EndRequest")
+        net.WriteBool(false)
+        net.WriteString(err)
+        net.SendToServer()
+    end)
 end
-
 
 function DemoRec.EndRecord()
-	RunConsoleCommand("stop")
-	DemoRec.recording = false
+    RunConsoleCommand("stop")
+    DemoRec.recording = false
+    local CurrentRecord = DemoRec.Queue[1] or DemoRec.CurrentRecord
+    table.remove(DemoRec.Queue, 1)
+    CurrentRecord.filename = CurrentRecord.file_prefix .. ".dem"
 
-	local CurrentRecord = DemoRec.Queue[1] or DemoRec.CurrentRecord
-	table.remove(DemoRec.Queue, 1)
+    if not CurrentRecord then
+        return
+    end
 
-	CurrentRecord.filename = CurrentRecord.file_prefix .. ".dem"
-
-	if not CurrentRecord then return end
-
-	timer.Simple(10, function()
-
-		if file.Exists(CurrentRecord.filename, "DATA") then
-			local data = file.Read(CurrentRecord.filename, "DATA")
-			file.Delete(CurrentRecord.filename)
-			DemoRec.PostDemo(data, CurrentRecord)
-		end
-
-	end )
-
+    timer.Simple(10, function()
+        if file.Exists(CurrentRecord.filename, "DATA") then
+            local data = file.Read(CurrentRecord.filename, "DATA")
+            file.Delete(CurrentRecord.filename)
+            DemoRec.PostDemo(data, CurrentRecord)
+        end
+    end)
 end
 
-
 function DemoRec.StartRecord()
-	if not DemoRec then return end
+    if not DemoRec then
+        return
+    end
 
-	if DemoRec.recording then
-		table.insert(DemoRec.Queue, DemoRec.CurrentRecord)
-		DemoRec.EndRecord()
-	end
+    if DemoRec.recording then
+        table.insert(DemoRec.Queue, DemoRec.CurrentRecord)
+        DemoRec.EndRecord()
+    end
 
-	DemoRec.CurrentRecord = {}
-	DemoRec.CurrentRecord.key = net.ReadUInt(18)
-	local length = net.ReadUInt(12)
-
-	DemoRec.CurrentRecord.file_prefix = net.ReadString()
-	DemoRec.settings.website = net.ReadString() .. "postdemo"
-
-	RunConsoleCommand("record", "data/" .. DemoRec.CurrentRecord.file_prefix)
-	DemoRec.recording = true
-
-	timer.Simple(length, DemoRec.EndRecord)
-
+    DemoRec.CurrentRecord = {}
+    DemoRec.CurrentRecord.key = net.ReadUInt(18)
+    local length = net.ReadUInt(12)
+    DemoRec.CurrentRecord.file_prefix = net.ReadString()
+    DemoRec.settings.website = net.ReadString() .. "postdemo"
+    RunConsoleCommand("record", "data/" .. DemoRec.CurrentRecord.file_prefix)
+    DemoRec.recording = true
+    timer.Simple(length, DemoRec.EndRecord)
 end
 
 net.Receive("DemoRec.StartRecord", DemoRec.StartRecord)
 
-
 hook.Add("Initialize", "DemoRec.Initialize", function()
-	hook.Remove("HUDPaint", "DrawRecordingIcon")
-end )
+    hook.Remove("HUDPaint", "DrawRecordingIcon")
+end)
--- lua/autorun/server/sv_demorec.lua
+++ lua/autorun/server/sv_demorec.lua
@@ -1,90 +1,86 @@
 util.AddNetworkString("DemoRec.StartRecord")
 util.AddNetworkString("DemoRec.SendSettings")
 util.AddNetworkString("DemoRec.EndRequest")
-
 DemoRec = DemoRec or {}
 DemoRec.settings = DemoRec.settings or {}
 DemoRec.requests = DemoRec.requests or {}
-
 include("../../demorec_settings.lua")
 
-
 function DemoRec.PlayerInitialSpawn(ply)
-   DemoRec.requests[ply:SteamID64()] = DemoRec.requests[ply:SteamID64()] or {}
+    DemoRec.requests[ply:SteamID64()] = DemoRec.requests[ply:SteamID64()] or {}
 end
 
 hook.Add("PlayerInitialSpawn", "DemoRec.PlayerInitialSpawn", DemoRec.PlayerInitialSpawn)
 
-
 function DemoRec:ChatNotify(ply, message)
-  if IsValid(ply) then
-     ply:ChatPrint("[DemoRec] " .. message)
-  end
+    if IsValid(ply) then
+        ply:ChatPrint("[DemoRec] " .. message)
+    end
 end
-
 
 function DemoRec:HasPermission(ply)
-  return ply:IsSuperAdmin() or (DemoRec.settings.admin_allowed and ply:IsAdmin())
+    return ply:IsSuperAdmin() or (DemoRec.settings.admin_allowed and ply:IsAdmin())
 end
 
-
 function DemoRec.ConCommand(ply, cmd, args)
-  if IsValid(ply) and not DemoRec:HasPermission(ply) then return end
+    if IsValid(ply) and not DemoRec:HasPermission(ply) then
+        return
+    end
 
-  local ident = args[1]
-  local target = player.GetBySteamID64(ident)
-  if not IsValid(target) then return end
+    local ident = args[1]
+    local target = player.GetBySteamID64(ident)
 
-  local length = tonumber(args[2])
-  if length <= 0 or length > DemoRec.settings.MaxLength then return end
+    if not IsValid(target) then
+        return
+    end
 
-  DemoRec:RequestDemo(target, length)
-  table.insert(DemoRec.requests[target:SteamID64()], ply)
-  DemoRec:ChatNotify(ply, "Requested demo from " .. target:Name() .. ".")
+    local length = tonumber(args[2])
+
+    if length <= 0 or length > DemoRec.settings.MaxLength then
+        return
+    end
+
+    DemoRec:RequestDemo(target, length)
+    table.insert(DemoRec.requests[target:SteamID64()], ply)
+    DemoRec:ChatNotify(ply, "Requested demo from " .. target:Name() .. ".")
 end
 
 concommand.Add("demorec", DemoRec.ConCommand)
 
-
 function DemoRec:AddWebClient(sid64, cl_key, file_prefix)
-  local post_table = {}
-  post_table["sid64"] = sid64
-  post_table["filename"] = file_prefix .. ".dem"
-  post_table["cl_key"] = tostring(cl_key)
-  post_table["sv_key"] = DemoRec.settings.sv_key
-
-  http.Post(DemoRec.settings.website .. "addclient", post_table)
-
+    local post_table = {}
+    post_table["sid64"] = sid64
+    post_table["filename"] = file_prefix .. ".dem"
+    post_table["cl_key"] = tostring(cl_key)
+    post_table["sv_key"] = DemoRec.settings.sv_key
+    http.Post(DemoRec.settings.website .. "addclient", post_table)
 end
-
 
 function DemoRec:RequestDemo(ply, length)
-  local key = math.random(100000)
-  local file_prefix = os.date("%H_%M_%S__%d_%m_%Y", os.time())
-
-  DemoRec:AddWebClient(ply:SteamID64(), key, file_prefix)
-
-  net.Start("DemoRec.StartRecord")
-  net.WriteUInt(key, 18)
-  net.WriteUInt(length, 12)
-  net.WriteString(file_prefix)
-  net.WriteString(DemoRec.settings.website)
-  net.Send(ply)
+    local key = math.random(100000)
+    local file_prefix = os.date("%H_%M_%S__%d_%m_%Y", os.time())
+    DemoRec:AddWebClient(ply:SteamID64(), key, file_prefix)
+    net.Start("DemoRec.StartRecord")
+    net.WriteUInt(key, 18)
+    net.WriteUInt(length, 12)
+    net.WriteString(file_prefix)
+    net.WriteString(DemoRec.settings.website)
+    net.Send(ply)
 end
-  
-  
+
 function DemoRec.EndRequest(len, ply)
     local success = net.ReadBool()
+
     if #DemoRec.requests[ply:SteamID64()] > 0 then
-       for k, admin in ipairs(DemoRec.requests[ply:SteamID64()]) do
-          if success then
-            DemoRec:ChatNotify(player.GetBySteamID64(admin), "Demo successfully sent by " .. ply:Name() .. " to web server.")
-          else
-            local err = net.ReadString()
-            DemoRec:ChatNotify(player.GetBySteamID64(admin), "Error from .. " .. ply:Name() .. " while sending demo to web server :\n" .. err)
-          end
-       end
+        for k, admin in ipairs(DemoRec.requests[ply:SteamID64()]) do
+            if success then
+                DemoRec:ChatNotify(player.GetBySteamID64(admin), "Demo successfully sent by " .. ply:Name() .. " to web server.")
+            else
+                local err = net.ReadString()
+                DemoRec:ChatNotify(player.GetBySteamID64(admin), "Error from .. " .. ply:Name() .. " while sending demo to web server :\n" .. err)
+            end
+        end
     end
 end
-  
+
 net.Receive("DemoRec.EndRequest", DemoRec.EndRequest)
