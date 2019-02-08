65c3d676accadc74bbc4c0e654a1a3060599ec2d
Butanal <benji.sepe@gmail.com>
2019-02-08 13:41:50 +0100

    Remove deprecated debug option

--- lua/demorec_settings.lua
+++ lua/demorec_settings.lua
@@ -1,4 +1,3 @@
-DemoRec.settings.debug = false -- prints debug data to user chat
 DemoRec.settings.sv_key = "change_this_key" -- key used to authenticate against the web server
 DemoRec.settings.MaxLength = 2000 -- time limit (in seconds) for duration
 DemoRec.settings.website = "http://example.com:5000/" -- replace with domain or IP, same format with trailing slash
